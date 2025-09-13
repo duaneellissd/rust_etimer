baf879ebca1f38e8f9118ed571c52dba2c68ec21
Duane Ellis <duane@duaneellis.com>
2025-09-13 14:47:00 -0700

    Missed lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core;
+// cspell: words libc etimer, highres, 
+// cspell: words getnow, timeval, gettimeofday
+use libc::timeval;
+use libc::gettimeofday;
+
+static mut TIME_ZERO : u64 = 0;
+
+/// time_zero is when the application started
+/// This returns the number of microseconds since start
+pub fn etimer_highres_getnow() -> u64 {
+    let mut tv : libc::timeval;
+    unsafe {
+        libc::gettimeofday( &tv, core::ptr::null_mut() );
+    }
+    let mut answer : u64 = tv.tv_sec.wrapping_mul( 1000000 );
+    answer = answer.wrapping_add( tv.tv_usec );
+    unsafe {
+        if TIME_ZERO == 0 {
+            TIME_ZERO = answer
+        }
+        answer = answer.wrapping_sub( TIME_ZERO );
+    }
+    return answer
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn etimer_highres_to_usecs( time_stamp : u64 ) -> u64 {
+    // Other platforms may have different highres resolution.
+    // This platform the high res timer is microseconds.
+    // Thus we can just return the timevalue and be done with it.
+    return time_stamp;
 }
